e14a37bb7a3b9c8e1c98a63e2c86961f28388772
Michael Blakeley <mblakele@viv.ai>
2015-07-22 16:20:17 -0700

    Check for node in PATH before trying nvm

--- lib/hook-template.sh
+++ lib/hook-template.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 #
 
-# Bootstrap nvm environment, for GUI tools.
-# This assumes nvm was installed per-user in `~/.nvm`.
-if [ -z "$NVM_DIR" ]; then
-  export NVM_DIR="$HOME/.nvm"
-fi
-NVM_SH="$NVM_DIR/nvm.sh"
-if [ -x "$NVM_SH" ]; then
-  source "$NVM_SH"
-else
-  echo FATAL: nvm.sh at $NVM_SH is missing, unreadable, or not executable!
-  exit 1
+NODE=$(type -P node)
+if [ -z "$NODE" ]; then
+  # Bootstrap nvm environment, for GUI tools.
+  # This assumes nvm was installed per-user in `~/.nvm`.
+  if [ -z "$NVM_DIR" ]; then
+    export NVM_DIR="$HOME/.nvm"
+  fi
+  NVM_SH="$NVM_DIR/nvm.sh"
+  if [ -x "$NVM_SH" ]; then
+    source "$NVM_SH"
+  else
+    echo FATAL: nvm.sh at $NVM_SH is missing, unreadable, or not executable!
+    exit 1
+  fi
 fi
 
 # The path is a little awkward.
